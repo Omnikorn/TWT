58462d66e879233bd4397186b3f2f12f5a0137cb
Harith Albadry <harith.albadry@gmail.com>
2021-06-27 23:17:38 +0100

    added styling to menue item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,32 +10,32 @@ class Directory extends React.Component {
         this.state ={
             sections:[
                 {
-                  title: 'hats',
+                  title: 'HATS',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
                   linkUrl: 'shop/hats'
                 },
                 {
-                  title: 'jackets',
+                  title: 'JACKETS',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
                   linkUrl: 'shop/jackets'
                 },
                 {
-                  title: 'sneakers',
+                  title: 'SHOES',
                   imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                   id: 3,
                   linkUrl: 'shop/sneakers'
                 },
                 {
-                  title: 'womens',
+                  title: 'WOMENS',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
                   linkUrl: 'shop/womens'
                 },
                 {
-                  title: 'mens',
+                  title: 'MENS',
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
@@ -51,8 +51,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id}) =>(
-                        <MenuItem key={id} title={title} />
+                    this.state.sections.map(({title, imageUrl, id, size}) =>(
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
             </div>
